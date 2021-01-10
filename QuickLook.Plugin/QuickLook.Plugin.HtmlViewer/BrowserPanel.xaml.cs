44308f0db52da72a11d192a769efdccffd273fe4
Frank Becker <fb@mooflu.com>
2021-01-09 18:32:15 -0800

    When previewing svg via WebView2 add zoom and pan via https://github.com/ariutta/svg-pan-zoom Currently loaded via network. TODO: bundle

--- QuickLook.Plugin/QuickLook.Plugin.HtmlViewer/BrowserPanel.xaml.cs
+++ QuickLook.Plugin/QuickLook.Plugin.HtmlViewer/BrowserPanel.xaml.cs
@@ -12,17 +12,43 @@ namespace QuickLook.Plugin.HtmlViewer
     {
         private Uri _currentUri;
 
+        private static string svgWrapperBegin =
+            @"<html>
+<head>  
+   <meta charset=""utf-8"">
+   <script src=""https://cdn.jsdelivr.net/npm/svg-pan-zoom@3.5.0/dist/svg-pan-zoom.min.js""></script>
+   <script>
+      window.onload = function() {
+        window.zoomTiger = svgPanZoom('svg', {zoomScaleSensitivity: 0.5});
+      };
+    </script>
+    <style>SVG {width: 100%; height: 100%;}</style>
+</head>
+<body style=""margin: 0"">
+";
+        private static string svgWrapperEnd = "</body></html>";
+
         public BrowserPanel()
         {
             InitializeComponent();
         }
 
-        public void LoadFile(string path)
+        public async void LoadFile(string path)
         {
-            if (Path.IsPathRooted(path))
-                path = Helper.FilePathToFileUrl(path);  
-
-            Navigate(path);
+            if (path.ToLower().EndsWith(".svg"))
+            {
+                await webView.EnsureCoreWebView2Async();
+                string svgText = File.ReadAllText(path);
+                string htmlContent = $@"{svgWrapperBegin}{svgText}{svgWrapperEnd}";
+                // File.WriteAllText(path + ".html, htmlContent);
+                webView.NavigateToString(htmlContent);
+            }
+            else
+            {
+                if (Path.IsPathRooted(path))
+                    path = Helper.FilePathToFileUrl(path);
+                Navigate(path);
+            }
         }
 
         public void Navigate(string uri)
@@ -49,6 +75,8 @@ namespace QuickLook.Plugin.HtmlViewer
 
         void NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
         {
+            if (e.Uri.StartsWith("data:text")) return; // arriving via NavigateToString
+
             var newUri = new Uri(e.Uri);
             if (newUri != _currentUri)
             {
